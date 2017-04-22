9db43b45a44c30b13f941d1d04841fd12d9f8301
Ruslan Stepanov <r.stepanov@corp.nekki.ru>
2017-04-22 20:40:25 +0300

    Tale color

--- Assets/Scripts/Fish/FishTale.cs
+++ Assets/Scripts/Fish/FishTale.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FishTale : MonoBehaviour {
 
@@ -10,9 +11,13 @@ public class FishTale : MonoBehaviour {
 
 	const float _maxAngle = 45;
 
+	Color _baseColor;
+
+	float _tailColorCoef = 0.2f;
+
 	// Use this for initialization
 	void Start () {
-		
+		_baseColor = gameObject.GetComponent<UnityEngine.UI.Image> ().color;
 	}
 	
 	// Update is called once per frame
@@ -26,6 +31,15 @@ public class FishTale : MonoBehaviour {
 			}
 
 			transform.Rotate(0,_deltaRot * Time.deltaTime,0);
+
+			UpdateColor ();
 		}
+	}
+
+	void UpdateColor ()
+	{
+		float angleY = transform.localRotation.eulerAngles.y;
+		float colorValue = (float)(angleY + _maxAngle) / (2 * _maxAngle) * _tailColorCoef;
+		gameObject.GetComponent<UnityEngine.UI.Image> ().color = new Color(_baseColor.r - colorValue, _baseColor.g - colorValue, _baseColor.b - colorValue);
 	}
 }
