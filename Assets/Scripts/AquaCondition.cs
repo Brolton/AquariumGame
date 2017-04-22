7cdc36e8e00c197c7a273d2aab84f6d207ff3d5a
Comanche_Ak <comanche.ak@gmail.com>
2017-04-22 21:35:44 +0300

    Aquarium Conditions added

---
+++ Assets/Scripts/AquaCondition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AquaCondition : MonoBehaviour {
+	public bool powerIsOn = true;
+	public float currentPower;
+	public float fullPower = 250f;
+	public float waterTemperature = 10f;
+	public float pumpPower = 20f;
+	public float oxygenPower = 20f;
+	public float heatPower = 20f;
+	public float lightPower = 20f;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentPower = pumpPower + oxygenPower + heatPower + lightPower;
+
+		if (currentPower > fullPower) {
+			powerIsOn = false;
+		}
+	}
+}
