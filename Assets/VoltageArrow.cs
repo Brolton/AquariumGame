eb56b10debe0727c36a22d70f11613d5a11ad242
Comanche_Ak <comanche.ak@gmail.com>
2017-04-24 10:39:24 +0300

    voltmeter works now

---
+++ Assets/VoltageArrow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoltageArrow : MonoBehaviour {
+	public GameObject mainController;
+	public float zAngle;
+	public float currentPower;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (mainController != null) {
+			currentPower = mainController.GetComponent<AquaCondition> ().currentPower;
+			zAngle = -currentPower/3.25f;
+			gameObject.GetComponent<RectTransform>().localRotation = Quaternion.Euler (new Vector3(0,0,zAngle));
+		}
+	}
+}
